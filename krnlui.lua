19a114982ad7d92688270e9c53d5be782d477017
ConeCat <192470872+Roblox-HttpSpy@users.noreply.github.com>
2026-07-08 18:26:28 +0100

    Revert "Test2"

--- krnlui.lua
+++ krnlui.lua
@@ -1,4 +1,3 @@
--- sadly doesn't work anymore
 local CoreGui, hui = game:GetService("CoreGui"), gethui()
 local StarterGui = game:GetService("StarterGui")
 local CorePackages = game:GetService("CorePackages")
